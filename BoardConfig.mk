787404cec5ae8625c911d2815ff41ec8ae631990
hak86 <hakamelassouad@gmail.com>
2016-10-17 01:46:10 +0200

    hi6210sft: add initial sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,14 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_FSTAB := device/huawei/hi6210sft/ramdisk/fstab.hi6210sft
 TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
 
+# Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/huawei/hi6210sft/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    	file_contexts \
+    	installd.te
+
 # Shell
 TARGET_SHELL := ash
 
