79aa1cae21dc0de87cd66efda950beefea3d097b
hak86 <hakamelassouad@gmail.com>
2016-10-17 02:32:36 +0200

    hi6210sft: add missed flag to copy audio etc

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -14,6 +14,7 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Audio
+PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/etc/audio_effects.conf:system/etc/audio_effects.conf \
 	$(LOCAL_PATH)/rootdir/etc/audio_policy.conf:system/etc/audio_policy.conf \
 	$(LOCAL_PATH)/rootdir/etc/hisi_omx.cfg:system/etc/hisi_omx.cfg \
